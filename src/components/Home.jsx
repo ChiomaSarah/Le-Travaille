ace58e88b4f39cf1d99ae390de24ef5e98407cb0
ChiomaSarah <osujisarah@gmail.com>
2022-12-25 17:02:19 +0000

    fixed a typo in home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,7 +5,7 @@ const Home = () => {
     <div className="text-center mt-5 pt-5">
       <h2 className="mt-5">Launch your career with us.</h2>
       <h6 className="mt-5">Let's help you get started. </h6>
-      <p className="mt-5">create an account and login to view open positions  .</p>
+      <p className="mt-5">Create an account and login to view open positions  .</p>
     </div>
   );
 };
